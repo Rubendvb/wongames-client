078ee0cde231411cbc2c2e7057a5f446db8b3909
Rubendvb <rubenvasquez556@gmail.com>
2024-08-15 23:42:02 -0300

    logo responsiveness and media queries

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react'
-
 import { renderWithRender } from '../../utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -35,5 +35,15 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithRender(<Logo $hideOnMobile />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width:  768px)' } // Um espaço a mais antes do 768px
+    )
   })
 })
