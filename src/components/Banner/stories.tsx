4e043d421bdd49e82ef8b3763c7d2c2022458334
Rubendvb <rubenvasquez556@gmail.com>
2024-08-24 05:14:56 -0300

    creating structure with TDD

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -6,11 +6,14 @@ const meta: Meta<typeof Banner> = {
   title: 'Banner',
   component: Banner,
   args: {
-    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    img: 'https://plus.unsplash.com/premium_photo-1724151998262-75576ac2b098?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Defy death',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 }
 
--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -12,7 +12,7 @@ const props = {
 }
 describe('<Banner />', () => {
   it('should render the heading', () => {
-    renderWithRender(<Banner {...props} />)
+    const { container } = renderWithRender(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /defy death/i })
@@ -23,5 +23,7 @@ describe('<Banner />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
